0382f596b65a30a577bf1cab72ab1caae6107fba
TakSig <48373638+TakSig@users.noreply.github.com>
2019-03-10 10:18:48 +0900

    Update RS-WFIREX3.rb

--- RS-WFIREX3.rb
+++ RS-WFIREX3.rb
@@ -10,15 +10,24 @@
 require 'socket'
 
 def get_RS_WFIREX3(ip)
-  TCPSocket.open(ip, 60001) { |socket|
-    socket.puts "\xAA\x00\x01\x18\x50"
-    res  = socket.gets.to_s.unpack("H*").to_s
-    temp = res[16,2].to_s.to_i(16)/10.0
-    rh   = res[10,4].to_s.to_i(16)/10.0
-    lux  = res[20,2].to_s.to_i(16)
-    [ temp, rh, lux ]
-  }
+  begin
+    temp, rh, lux = TCPSocket.open(ip, 60001) { |socket|
+      socket.puts "\xAA\x00\x01\x18\x50"
+      res  = socket.gets.to_s.unpack("H*").to_s
+      temp = res[16,2].to_s.to_i(16)/10.0
+      rh   = res[10,4].to_s.to_i(16)/10.0
+      lux  = res[20,2].to_s.to_i(16)
+      [ temp, rh, lux ]
+    }
+  rescue Exception => exc
+    $stderr.puts exc
+    temp = nil
+    rh   = nil
+    lux  = nil
+  end
+  [ temp, rh, lux ]
 end
+
 
 # call with IP address of RS-WFIREX3 at your environment
 ip = "192.168.1.***"
